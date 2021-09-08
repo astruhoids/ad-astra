c825c7c277a7baf9833254989a0a64e3ac749f99
deshay-clemons <54691651+deshay-clemons@users.noreply.github.com>
2021-09-07 17:00:44 -1000

    #1 Rough Draft of Landing Page
    - Mimicked LumiSight Landing Page
    - Imported images needed for website
    - Added a little bit of CSS

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
-import { Container, Col, Image, Row } from 'react-bootstrap';
+import { Container, Col, Row, Button, Image } from 'react-bootstrap';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-      <Container id='landing-page'>
-        <Row>
-          <Col xs={4}>
-            <Image style={{ width: "200px", height: "auto" }} fluid roundedCircle src="/images/meteor-logo.png"/>
-          </Col>
-          <Col xs={8} className="text-center my-auto">
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Col>
-        </Row>
+      <Container id="bg-image" className="d-flex" fluid>
+        <Container id='landing-page' className="text-center justify-content-center align-self-center">
+          <Row>
+            <Col>
+              <Image style={{ width: '100px' }} roundedCircle src="/images/amongus-yellow-logo.jpg"/>
+              <h1>Ad Astra</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Button className="landing-btns" variant="primary" size="lg">Login</Button>
+              <Button className="landing-btns" variant="secondary" size="lg">New User?</Button>
+            </Col>
+          </Row>
+        </Container>
       </Container>
     );
   }
