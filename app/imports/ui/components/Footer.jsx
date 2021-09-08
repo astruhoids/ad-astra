be4bd333ac75c754caf33e9d8371d7e8f797ffa4
Kevin Nguyen <nk279@hawaii.edu>
2021-09-07 22:32:12 -1000

    added logo, edited footer, fixed navbar dropdown going off screen

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -8,10 +8,9 @@ class Footer extends React.Component {
       <footer>
         <Container fluid style={divStyle} className="text-center">
           <hr />
-          Department of Information and Computer Sciences <br />
-          University of Hawaii<br />
+          AstrUHoids <br />
+          University of Hawaii <br />
           Honolulu, HI 96822 <br />
-          <a href="http://ics-software-engineering.github.io/meteor-application-template-react">Template Home Page</a>
         </Container>
       </footer>
     );
