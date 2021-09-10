20291077488c0c02158cdfec6c69be1b91f7b67d
Deshay Clemons <dclemons@hawaii.edu>
2021-09-09 20:47:58 -1000

    #6 Created DailyCheckup Page
    - Alert Clearance links to DailyCheckup Page
    - Hide Alert works

--- app/imports/ui/components/Clearance.jsx
+++ app/imports/ui/components/Clearance.jsx
@@ -1,36 +1,59 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Alert, Button, Container } from 'react-bootstrap';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Clearance extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { cleared: false, status: 'IMPOSTER', variant: 'warning', hidden: 'true' };
+    this.state = { cleared: false, status: 'IMPOSTER', variant: 'warning', hidden: false };
     this.handleCheck = this.handleCheck.bind(this);
+    this.handleDismiss = this.handleDismiss.bind(this);
+
   }
 
   handleCheck() {
     this.setState({ cleared: true, status: 'CREWMATE', variant: 'success' });
   }
 
+  handleDismiss() {
+    if (this.state.hidden === true) {
+      this.setState({ hidden: false });
+    } else if (this.state.hidden === false) {
+      this.setState({ hidden: true });
+    }
+  }
+
   render() {
     return (
-      <Alert style={{ marginBottom: '0px' }} variant={this.state.variant} hidden={this.state.hidden} dismissible>
-        <Alert.Heading>Status - {this.state.status}:</Alert.Heading>
-        {this.state.cleared ?
-          <Container>
-            <p>
-              You may report to campus / Anyone in Quarantine MUST continue to adhere to location restrictions
-            </p>
-          </Container>
+      <div className="d-flex justify-content-center fluid" style={{ position: 'absolute', marginLeft: 'auto', marginRight: 'auto', left: 0, right: 0 }}>
+        {this.state.hidden ?
+          <Alert style={{ marginBottom: '0px' }} variant={this.state.variant} onClose={this.handleDismiss} dismissible>
+            <Alert.Heading>Status - {this.state.status}:</Alert.Heading>
+            <Container>
+              {this.state.cleared ?
+                <Container>
+                  <p>
+                    You may report to campus / Anyone in Quarantine MUST continue to adhere to location restrictions
+                  </p>
+                </Container>
+                :
+                <Container>
+                  <p>
+                    You have not completed the safety requirements
+                  </p>
+                  <Button variant="Link" onClick={this.handleCheck}>
+                    <Link to={{ pathname: '/dailycheck' }}>
+                      Check your Symptoms
+                    </Link>
+                  </Button>
+                </Container>
+              }
+            </Container>
+          </Alert>
           :
-          <Container>
-            <p>
-              You have not completed the safety requirements
-            </p>
-            <Button onClick={this.handleCheck}>Check your Symptoms</Button>
-          </Container>
+          <Button onClick={this.handleDismiss}>Show Alert</Button>
         }
-      </Alert>
+      </div>
     );
   }
 }
---
+++ app/imports/ui/pages/DailyCheckup.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { Button, Container, Row, Col } from 'react-bootstrap';
+// import Clearance from '../components/Clearance';
+
+/** A simple static component to render some text for the landing page. */
+class DailyCheckup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { choice: null, redirectToReferer: false };
+  }
+
+  submit(e) {
+    e = e == 'true' ? true : false;
+
+    this.setState({ choice: e, redirectToReferer: true });
+  }
+
+  render() {
+    // if correct authentication, redirect to page instead of login screen
+    if (this.state.redirectToReferer) {
+      return <Redirect to={{ pathname: '/' }}/>;
+    }
+
+    return (
+      <div>
+        {/* <Clearance /> */}
+        <Container id="bg-image" className="d-flex" fluid>
+          <Container style={{ backgroundColor: 'whitesmoke', padding: '2.5rem', marginTop: '5rem', marginBottom: '5rem' }}>
+            <Row>
+              <h1>Do any of the following apply to you?</h1>
+            </Row>
+            <Row>
+              <Container>
+                <p>
+                  Have you tested positive for COVID-19 and are on home isolation?
+                </p>
+                <p>
+                  Check for Symptoms of Illness:  If you have any symptoms of illness,
+                  do not come to campus or the workplace.  Do you currently have any of
+                  the following symptoms that are <strong> new, worsening, and not attributable to a pre-existing condition?</strong>
+                </p>
+                <ul>
+                  <li>
+                    Fever greater than 100.4 °F or feeling feverish (chills, sweating)
+                  </li>
+                  <li>
+                    Cough
+                  </li>
+                  <li>
+                    Shortness of breath/difficulty breathing
+                  </li>
+                  <li>
+                    Sore throat
+                  </li>
+                  <li>
+                    Unexplained muscle/body aches
+                  </li>
+                  <li>
+                    Nausea/vomiting or diarrhea
+                  </li>
+                  <li>
+                    Loss of senses of taste or smell
+                  </li>
+                  <li>
+                    Runny or congested nose
+                  </li>
+                  <li>
+                    Headache
+                  </li>
+                  <li>
+                    Skin rash
+                  </li>
+                  <li>
+                    Chest pain or pressure
+                  </li>
+                </ul>
+                <p>
+                  Check for Recent COVID-19 Exposure:
+                </p>
+                <ul>
+                  <li>
+                    Have you traveled out of the state and are currently under quarantine orders by the Department of Health or your medical care provider ?
+                  </li>
+                  <li>
+                    Are you unvaccinated and have been in close contact (&lt;6 feet for &ge; 15 minutes, cumulatively, over a 24-hour period) with anyone who
+                    has an active, diagnosed case of COVID-19?  Note: Healthcare students/personnel wearing appropriate PPE at ALL TIMES while caring for a patient with COVID-19 would NOT be considered a close contact (ref. DOH medical advisory #16)
+                  </li>
+                  <li>
+                    Has the Department of Health told you that you have been in contact with a person with COVID-19 AND you are UNvaccinated?
+                  </li>
+                </ul>
+              </Container>
+            </Row>
+            <Row>
+              <Col>
+                <Button variant='dark' className='landing-btns' value={true} onClick={e => this.submit(e.target.value)}>Yes</Button>
+              </Col>
+              <Col>
+                <Button variant='dark' className='landing-btns' value={false} onClick={e => this.submit(e.target.value)}>No</Button>
+              </Col>
+            </Row>
+          </Container>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default DailyCheckup;
