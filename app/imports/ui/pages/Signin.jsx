398c5d48e5313537006003a61eff043239fbd14b
Gabriel Undan <gundan@hawaii.edu>
2021-09-12 13:57:05 -1000

    Signin page functional

--- app/imports/ui/pages/Signin.jsx
+++ app/imports/ui/pages/Signin.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link, Redirect } from 'react-router-dom';
 import { Meteor } from 'meteor/meteor';
-// import { Container, Form, Grid, Header, Message, Segment } from 'semantic-ui-react';
-import { Container, Col, Row, Form, Button, Alert, Card, InputGroup } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import { AutoForm, SubmitField, TextField } from 'uniforms-bootstrap4';
+import { Container, Col, Row, Alert, Card } from 'react-bootstrap';
+import SimpleSchema from 'simpl-schema';
+
+const bridge = new SimpleSchema2Bridge(new SimpleSchema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true,
+    uniforms: { type: 'password' }
+  }
+}));
 
 /**
  * Signin page overrides the form’s submit event and call Meteor’s loginWithPassword().
@@ -19,15 +31,10 @@ export default class Signin extends React.Component {
     this.state = { email: '', password: '', error: '', redirectToReferer: false };
   }
 
-  // Update the form controls each time the user interacts with them.
-  handleChange = (e, { name, value }) => {
-    console.log(e);
-    this.setState({ [name]: value });
-  }
-
   // Handle Signin submission using Meteor's account mechanism.
-  submit = () => {
-    const { email, password } = this.state;
+  submit = (data, fRef) => {
+    const { password, email } = data;
+    
     Meteor.loginWithPassword(email, password, (err) => {
       if (err) {
         this.setState({ error: err.reason });
@@ -45,6 +52,7 @@ export default class Signin extends React.Component {
       return <Redirect to={from}/>;
     }
     // Otherwise return the Login form.
+    let fRef = null;
     return (
       <Container id="bg-image" className="d-flex" fluid>
         <Container id="signin-page">
@@ -53,40 +61,11 @@ export default class Signin extends React.Component {
               <Card>
                 <Card.Header className="text-center">Account Login</Card.Header>
                 <Card.Body>
-                  <Form onSubmit={this.submit}>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                      <Form.Label>Email address</Form.Label>
-                      <InputGroup>
-                        <InputGroup.Text className="no-right-radius">
-                          <FontAwesomeIcon icon={faUser} />
-                        </InputGroup.Text>
-                        <Form.Control 
-                          name="email"
-                          type="email"
-                          placeholder="E-mail address"    
-                          onChange={this.handleChange}
-                          />
-                      </InputGroup>
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                      <Form.Label>Password</Form.Label>
-                      <InputGroup>
-                        <InputGroup.Text className="no-right-radius">
-                          <FontAwesomeIcon icon={faLock} />
-                        </InputGroup.Text>
-                        <Form.Control
-                          name="password"
-                          type="password"
-                          placeholder="Password"
-                          onChange={this.handleChange}
-                          />
-                      </InputGroup>
-                    </Form.Group>
-                    <Button variant="secondary" type="submit" className="pl-3 pr-3">
-                      Login
-                    </Button>
-                  </Form>
+                  <AutoForm ref={ref => {fRef = ref}} schema={bridge} onSubmit={data => this.submit(data, fRef)}>
+                    <TextField type="email" name="email"/>
+                    <TextField type="password" name="password"/>
+                    <SubmitField inputClassName="btn btn-secondary pl-3 pr-3" value="Login"/>
+                  </AutoForm>
                 </Card.Body>
               </Card>
               <Alert variant="secondary" className="mt-3">
@@ -95,7 +74,7 @@ export default class Signin extends React.Component {
               {this.state.error === '' ? (
                 ''
               ) : (
-                <Alert variant="danger" className="pl-0">
+                <Alert variant="danger" className="">
                   Login was not successful: { this.state.error }
                 </Alert>
               )}
@@ -104,52 +83,6 @@ export default class Signin extends React.Component {
         </Container>
       </Container>
     );
-      // <Container id="signin-page">
-      //   <Grid textAlign="center" verticalAlign="middle" centered columns={2}>
-      //     <Grid.Column>
-      //       <Header as="h2" textAlign="center">
-      //         Login to your account
-      //       </Header>
-      //       <Form onSubmit={this.submit}>
-      //         <Segment stacked>
-      //           <Form.Input
-      //             label="Email"
-      //             id="signin-form-email"
-      //             icon="user"
-      //             iconPosition="left"
-      //             name="email"
-      //             type="email"
-      //             placeholder="E-mail address"
-      //             onChange={this.handleChange}
-      //           />
-      //           <Form.Input
-      //             label="Password"
-      //             id="signin-form-password"
-      //             icon="lock"
-      //             iconPosition="left"
-      //             name="password"
-      //             placeholder="Password"
-      //             type="password"
-      //             onChange={this.handleChange}
-      //           />
-      //           <Form.Button id="signin-form-submit" content="Submit"/>
-      //         </Segment>
-      //       </Form>
-      //       <Message>
-      //         <Link to="/signup">Click here to Register</Link>
-      //       </Message>
-      //       {this.state.error === '' ? (
-      //         ''
-      //       ) : (
-      //         <Message
-      //           error
-      //           header="Login was not successful"
-      //           content={this.state.error}
-      //         />
-      //       )}
-      //     </Grid.Column>
-      //   </Grid>
-      // </Container>
   }
 }
 
