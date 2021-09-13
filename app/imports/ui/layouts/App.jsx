65c5e68d0543f2429fdb49bd82a355ce2612f004
Gabriel Undan <gundan@hawaii.edu>
2021-09-12 15:55:26 -1000

    Added /signup and /signout routes

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -15,8 +15,8 @@ import Clearance from '../components/Clearance';
 // import EditStuff from '../pages/EditStuff';
 // import NotFound from '../pages/NotFound';
 import Signin from '../pages/Signin';
-// import Signup from '../pages/Signup';
-// import Signout from '../pages/Signout';
+import Signup from '../pages/Signup';
+import Signout from '../pages/Signout';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 class App extends React.Component {
@@ -30,6 +30,8 @@ class App extends React.Component {
             <Route exact path="/" component={Landing}/>
             <Route exact path="/dailycheck" component={DailyCheckup}/>
             <Route exact path="/login" component={Signin}/>
+            <Route path="/signup" component={Signup}/>
+            <Route path="/signout" component={Signout}/>
             {/* <Route path="/signin" component={Signin}/>
             <Route path="/signup" component={Signup}/>
             <Route path="/signout" component={Signout}/>
