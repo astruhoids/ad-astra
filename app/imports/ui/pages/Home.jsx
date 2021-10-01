c3ed374a7150fad2c0706856dc4eb9d9ac9ba115
Gabriel Undan <gundan@hawaii.edu>
2021-09-30 23:35:52 -1000

    Added ready check before rendering clearence

--- app/imports/ui/pages/Home.jsx
+++ app/imports/ui/pages/Home.jsx
@@ -20,7 +20,7 @@ class Home extends React.Component {
     return (
       <Container fluid>
         <Container id="home-page">
-          <Clearance statuses={this.props.health}/>
+          {(this.props.ready) ? (<Clearance statuses={this.props.health}/>) : <></>}
           <Row xs={1}>
             <Col>
               <h1 style={{ color: 'white' }}>On-campus check-in</h1>
